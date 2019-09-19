8d805f3519d682f67e8f771881db5786114f2412
k_maryam27 <k_maryam27@yahoo.com>
2019-09-19 01:38:28 -0400

    spring initializer with web mysql jpa

---
+++ gitars-api/src/main/java/com/galvanize/gitarsapi/GitarsApiApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.gitarsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitarsApiApplication.class, args);
+	}
+
+}
