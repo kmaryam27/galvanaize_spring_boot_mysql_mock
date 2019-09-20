f70949d3611687d3143ed6d400b85a9234233828
k_maryam27 <k_maryam27@yahoo.com>
2019-09-20 11:46:32 -0400

    simple controller test with mockMVC

--- gitars-api/src/main/java/com/galvanize/gitarsapi/controllers/GitarController.java
+++ gitars-api/src/main/java/com/galvanize/gitarsapi/controllers/GitarController.java
@@ -3,25 +3,24 @@ package com.galvanize.gitarsapi.controllers;
 import com.galvanize.gitarsapi.entities.Gitar;
 import com.galvanize.gitarsapi.services.GitarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Transactional
-@RequestMapping("/")
+@RequestMapping("/gitars")
 public class GitarController {
     @Autowired
     private GitarService gitarService;
 
+
     @GetMapping
-    public String getAllGitars(){
-        List<Gitar> list = gitarService.getAllGitars();
-        return (list.size() + "");
+//    @ResponseStatus(HttpStatus.OK)
+    public List<Gitar> getAll(){
+        return gitarService.getAllGitars();
     }
 
     @GetMapping(value="/{id}")
--- gitars-api/src/test/java/com/galvanize/gitarsapi/controllers/GitarControllerTest.java
+++ gitars-api/src/test/java/com/galvanize/gitarsapi/controllers/GitarControllerTest.java
@@ -1,16 +1,65 @@
 package com.galvanize.gitarsapi.controllers;
-
+import com.galvanize.gitarsapi.entities.Gitar;
+import com.galvanize.gitarsapi.repositories.GitarRepository;
+import com.galvanize.gitarsapi.services.GitarService;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@Transactional
+//@WebMvcTest(GitarController.class)
 public class GitarControllerTest {
+    @Autowired
+    MockMvc mockMvc;
 
-    @Test
-    public void getAllGitars() {
+    @Autowired
+    GitarService gitarService;
+    @Autowired
+    GitarRepository gitarRipository;
+
+
+
+    List<Gitar> gitarList = new ArrayList<>();
+    @Before
+    public void setUp() throws Exception {
+        // clean out the test db
+        List<Gitar> myList = gitarRipository.findAll();
+
+        myList.stream()
+                .forEach(gitar -> gitarRipository.deleteById(gitar.getId()));
+
+
+        for (int i=0; i< 10; i++){
+            Gitar gitar = new Gitar("model"+i,"brand"+i,i*2);
+            gitarList.add(gitar);
+        }
+        gitarRipository.saveAll(gitarList);
+
     }
 
     @Test
-    public void getGitarById() {
+    public void getAll() throws Exception {
+
+        this.mockMvc.perform(get("/gitars"))
+                .andExpect(status().isOk());
     }
+
 }
